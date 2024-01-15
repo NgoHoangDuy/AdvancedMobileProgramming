ec1dde5245f58cc9cbc25a222c4ec869ef604ff2
Ngô Hoàng Duy <ngohoangduy20@gmail.com>
2024-01-15 22:21:35 +0700

    codebase, global variable, init screens

---
+++ eventhub/src/navigators/AuthNavigator.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const AuthNavigator = () => {
+  return (
+    <View>
+      <Text>AuthNavigator</Text>
+    </View>
+  );
+};
+
+export default AuthNavigator;
